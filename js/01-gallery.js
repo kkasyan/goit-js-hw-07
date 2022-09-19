121a499dbe86b0d64a7c5ac5feced0b87eb5c0df
Kateryna Kasian <kkasyan1@gmail.com>
2022-09-19 22:32:14 +0300

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const imageListEl = galleryItems.map(({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
+    />
+  </a>
+</div>`;
+});
+
+let markup = imageListEl.join("");
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick({ target }) {
+  event.preventDefault();
+  //   window.addEventListener("keydown", onEscapePress);
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src='${target.dataset.source}' width="800" height="600">
+`);
+  {
+    onShow: (instance) => {
+      document.addEventListener("keydown", (evt) => {
+        if (evt.key === "Escape") {
+          return instance.close();
+        }
+      });
+    };
+    onClose: (instance) => {
+      document.addEventListener("keydown", (evt) => {
+        if (evt.key === "Escape") {
+          return instance.close();
+        }
+      });
+    };
+  }
+  //   function onEscapePress(evt) {
+  //     if (evt.key === "Escape") {
+  //       instance.close();
+  //       window.removeEventListener("keydown", onEscapePress);
+  //       console.log(event.key);
+  //     }
+  //   }
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const imageListEl = galleryItems.map(({ preview, original, description }) => {
+  return `<a class="gallery__item" href='${original}'>
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+});
+
+let markup = imageListEl.join("");
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+}
+
+const simple = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
